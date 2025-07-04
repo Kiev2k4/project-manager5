d8b5e58807f3fe62a69539bfed73e5fec495eaa7
Hau <s3978175@rmit.edu.vn>
2025-07-04 19:34:50 +0700

    Hau connecting Frontend to Backend

--- frontend/app/routes/auth/auth-layout.tsx
+++ frontend/app/routes/auth/auth-layout.tsx
@@ -1,8 +1,18 @@
+import { useAuth } from '@/provider/auth-context';
 import React from 'react'
-import { Outlet } from 'react-router';
+import { Outlet, Navigate } from 'react-router';
 
 const AuthLayout = () => {
-  return <Outlet />
+  const {isAuthenticated, isLoading} = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+  return <Outlet />;
 }
 
 export default AuthLayout;
