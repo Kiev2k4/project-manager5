708eb8589cb9db8008855fcd6e2e8dffe54991a6
Hau <s3978175@rmit.edu.vn>
2025-07-04 22:07:07 +0700

    Hau set up backend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", async (req, res) => {
+    res.status(200).json({
+        message: "Welcome to TaskHub API",
+    });
+});
+
+// error middleware
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({message: "Internal server error"});
+});
+
+// not found middleware
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Not found",
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
