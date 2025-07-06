0ad428b81336de5bb4e2918ffb84807fa70e3fc4
Hau <s3978175@rmit.edu.vn>
2025-07-06 23:49:29 +0700

    Hau done login schema

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,3 +1,4 @@
 const registerUser = async (req, res) => {};
+const loginUser = async (req, res) => {};
 
-export {registerUser};
+export {registerUser, loginUser};
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,25 @@
 import express from "express";
 import {z} from "zod";
 import {validateRequest} from "zod-express-middleware";
-import { registerSchema } from "../libs/validate-schema.js";
-import { registerUser } from "../controllers/auth-controller.js";
+import { loginSchema, registerSchema } from "../libs/validate-schema.js";
+import { registerUser, loginUser } from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
 router.post(
     "/register",
     validateRequest({
-        body: registerSchema
+        body: registerSchema,
     }),
     registerUser
+);
+
+router.post(
+    "/login",
+    validateRequest({
+        body: loginSchema,
+    }),
+    loginUser
 );
 
 export default router;
