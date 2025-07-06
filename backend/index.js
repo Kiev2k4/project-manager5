106a82f6c11bde92a563f226ee048d849841d379
Hau <s3978175@rmit.edu.vn>
2025-07-06 23:46:16 +0700

    Hau done auth

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,19 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
+import routes from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: ['Content-Type', "Authorization"],
-}));
+app.use(
+    cors({
+        origin:process.env.FRONTEND_URL,
+        methods: ["GET", "POST", "DELETE", "PUT"],
+        allowedHeaders: ['Content-Type', "Authorization"],
+    })
+);
 app.use(morgan("dev"));
 
 //db connection
@@ -30,6 +33,9 @@ app.get("/", async (req, res) => {
         message: "Welcome to TaskHub API",
     });
 });
+
+//http:localhost:500/api-v1/
+app.use("/api-v1", routes);
 
 // error middleware
 app.use((err, req, res, next) => {
