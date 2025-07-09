90596e50bfeaee7fb2a0f5c29bc86c463f11d282
Hau <s3978175@rmit.edu.vn>
2025-07-10 02:59:24 +0700

    Hau done Forgot Password & Reset User Password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,8 @@
 import express from "express";
 import {z} from "zod";
 import {validateRequest} from "zod-express-middleware";
-import { loginSchema, registerSchema, verifyEmailSchema } from "../libs/validate-schema.js";
-import { registerUser, loginUser, verifyEmail } from "../controllers/auth-controller.js";
+import { loginSchema, registerSchema, verifyEmailSchema, resetPasswordSchema, emailSchema } from "../libs/validate-schema.js";
+import { registerUser, loginUser, verifyEmail, resetPasswordRequest, verifyResetPasswordTokenAndResetPassword } from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
@@ -28,6 +28,22 @@ router.post(
         body: verifyEmailSchema,
     }),
     verifyEmail
+);
+
+router.post(
+    "/reset-password-request",
+    validateRequest({
+        body: emailSchema,
+    }), 
+    resetPasswordRequest
+);
+
+router.post(
+    "/reset-password",
+    validateRequest({
+        body: resetPasswordSchema,
+    }), 
+    verifyResetPasswordTokenAndResetPassword
 );
 
 export default router;
