336d88caf071ab0e76331871585975bcdef1cd04
Hau <s3978175@rmit.edu.vn>
2025-07-17 12:11:45 +0700

    Hau done dashboard-layout

--- frontend/app/routes/dashboard/dashboard-layout.tsx
+++ frontend/app/routes/dashboard/dashboard-layout.tsx
@@ -1,13 +1,50 @@
+import { Header } from "@/components/layout/header";
+import { SidebarComponent } from "@/components/layout/sidebar-component";
+import { Loader } from "@/components/loader";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/provider/auth-context";
+import type { Workspace } from "@/types";
+import { useState } from "react";
+import { Navigate, Outlet } from "react-router";
 
 const DashboardLayout = () => {
-  const { user, logout } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
+  const [isCreatingWorkspace, setIsCreatingWorkspace] = useState(false);
+  const [currentWorkspace, setCurrentWorkspace] = useState<Workspace | null>(
+    null
+  );
+
+  if (isLoading) {
+    console.log("[DashboardLayout] Loading...");
+    return <Loader />;
+  }
+
+  if (!isLoading && !isAuthenticated) {
+    console.log("[DashboardLayout] Not authenticated, redirecting to /sign-in");
+    return <Navigate to="/sign-in" />;
+  }
+
+  const handleWorkspaceSelected = (workspace: Workspace) => {
+    setCurrentWorkspace(workspace);
+  };
+
   return (
-    <div>
-      <Button onClick={logout}>Logout</Button>
+    <div className="flex h-screen w-full">
+      <SidebarComponent currentWorkspace={currentWorkspace} />
+      <div className="flex flex-1 flex-col h-full">
+        <Header
+          onWorkspaceSelected={handleWorkspaceSelected}
+          selectedWorkspace={currentWorkspace}
+          onCreateWorkspace={() => setIsCreatingWorkspace(true)}
+        />
+        <main className="flex-1 overflow-y-auto h-full w-full">
+          <div className="mx-auto container px-2 sm:px-6 lg:px-8 py-0 md:py-8 w-full h-full">
+            <Outlet />
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
 
-export default DashboardLayout
+export default DashboardLayout;
