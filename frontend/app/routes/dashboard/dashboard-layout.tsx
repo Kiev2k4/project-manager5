22a633e3da9b2784fd26ed0931c084a28b923f20
Hau <s3978175@rmit.edu.vn>
2025-07-17 13:49:16 +0700

    Hau done Create Workspace

--- frontend/app/routes/dashboard/dashboard-layout.tsx
+++ frontend/app/routes/dashboard/dashboard-layout.tsx
@@ -2,10 +2,21 @@ import { Header } from "@/components/layout/header";
 import { SidebarComponent } from "@/components/layout/sidebar-component";
 import { Loader } from "@/components/loader";
 import { Button } from "@/components/ui/button";
+import { CreateWorkspace } from "@/components/workspace/create-workspace";
+import { fetchData } from "@/lib/fetch-util";
 import { useAuth } from "@/provider/auth-context";
 import type { Workspace } from "@/types";
 import { useState } from "react";
 import { Navigate, Outlet } from "react-router";
+
+export const clientLoader = async () => {
+  try {
+    const [workspaces] = await Promise.all([fetchData("/workspaces")]);
+    return { workspaces };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const DashboardLayout = () => {
   const { isAuthenticated, isLoading } = useAuth();
@@ -43,6 +54,10 @@ const DashboardLayout = () => {
           </div>
         </main>
       </div>
+      <CreateWorkspace
+        isCreatingWorkspace={isCreatingWorkspace}
+        setIsCreatingWorkspace={setIsCreatingWorkspace}
+      />
     </div>
   );
 };
